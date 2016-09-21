0e5f0894e5f52d7396221e2dabebaeafdb123771
Torn <ott.adermann@gmail.com>
2016-09-21 17:53:30 +0300

    Render is wider, looks nicer

--- src/main/Render.java
+++ src/main/Render.java
@@ -2,12 +2,12 @@ package main;
 
 public class Render {
 	static public void showBoard(int[][] board) {
-		String[] XO = {"X", "O", " "};
+		String[] XO = {" X ", " O ", "   "};
 		
 		for (int a = 0; a < 3; a++) {
 			System.out.println(XO[board[a][0]] + "│" + XO[board[a][1]] + "│" + XO[board[a][2]]);
 			if (a < 2) {
-				System.out.println("─┼─┼─");
+				System.out.println("───┼───┼───");
 			}
 		}
 	}
