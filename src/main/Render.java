72fb953f73387a385a4c6df4fd8bf958f8c06298
Torn <ott.adermann@gmail.com>
2016-09-21 17:21:07 +0300

    ASCII Render

---
+++ src/main/Render.java
@@ -0,0 +1,12 @@
+package main;
+
+public class Render {
+	public void showBoard(int[][] board) {
+		for (int a = 0; a < 3; a++) {
+			System.out.println(board[a][0] + "│" + board[a][1] + "│" + board[a][2]);
+			if (a < 2) {
+				System.out.println("─┼─┼─");
+			}
+		}
+	}
+}
