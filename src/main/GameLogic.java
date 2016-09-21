f612207c489505ff0f5421fc937e7954de3e15a6
Kristjan Laht <kristjan.laht13@gmail.com>
2016-09-21 17:37:40 +0300

    changed game to use -1 for empty places

--- src/main/GameLogic.java
+++ src/main/GameLogic.java
@@ -10,16 +10,16 @@ import static java.lang.Math.abs;
  */
 public class GameLogic {
     private static final int B_SIZE = 3;
+    private static final int EMPTY = -1;
+    private static final int PLAYER1 = 0;
+    private static final int PLAYER2 = 1;
+
 
     private int gameboard[][];
     private int curPlayer = 0;
 
     public int[][] getGameboard() {
         return gameboard;
-    }
-
-    public void setGameboard(int[][] gameboard) {
-        this.gameboard = gameboard;
     }
 
     public GameLogic(int curPlayer) {
@@ -33,10 +33,10 @@ public class GameLogic {
     }
 
     public int move(int[] p) {
-        if (gameboard[p[0]][p[1]] != 0) {
+        if (gameboard[p[0]][p[1]] != EMPTY) {
             throw new RuntimeException("Tried to play where somebody has played before");
         }
-        gameboard[p[0]][p[1]] = curPlayer + 1;
+        gameboard[p[0]][p[1]] = curPlayer;
         curPlayer = abs(curPlayer - 1);
 
         return checkWinner();
@@ -60,13 +60,13 @@ public class GameLogic {
     public int checkWinner() {
         // check horisontal
         for (int y = 0; y < B_SIZE; y++) {
-            int player1 = Arrays.stream(gameboard[y]).filter( cell -> cell == 1).sum();
-            int player2 = Arrays.stream(gameboard[y]).filter( cell -> cell == 2).sum();
+            int player1 = Arrays.stream(gameboard[y]).filter( cell -> cell == PLAYER1).sum();
+            int player2 = Arrays.stream(gameboard[y]).filter( cell -> cell == PLAYER2).sum();
             
             if (player1 == B_SIZE) {
-                return 1;
+                return PLAYER1;
             }else if (player2 == B_SIZE) {
-                return 2;
+                return PLAYER2;
             }
         }
         // check vertical
@@ -74,28 +74,35 @@ public class GameLogic {
             int player1 = 0;
             int player2 = 0;
             for (int y = 0; y < B_SIZE; y++) {
-                if (gameboard[y][x] == 1) {
+                if (gameboard[y][x] == PLAYER1) {
                     player1++;
-                }else if(gameboard[y][x] == 2) {
+                }else if(gameboard[y][x] == PLAYER2) {
                     player2++;
                 }
             }
+
+            if (player1 == B_SIZE) {
+                return PLAYER1;
+            }else if (player2 == B_SIZE) {
+                return PLAYER2;
+            }
+
         }
         // check diag1
         {
             int player1 = 0;
             int player2 = 0;
             for (int i = 0; i < B_SIZE; i++) {
-                if (gameboard[i][i] == 1) {
+                if (gameboard[i][i] == PLAYER1) {
                     player1++;
-                }else if(gameboard[i][i] == 2) {
+                }else if(gameboard[i][i] == PLAYER2) {
                     player2++;
                 }
             }
             if (player1 == B_SIZE) {
-                return 1;
+                return PLAYER1;
             }else if (player2 == B_SIZE) {
-                return 2;
+                return PLAYER2;
             }
         }
         // check diag2
@@ -103,16 +110,16 @@ public class GameLogic {
             int player1 = 0;
             int player2 = 0;
             for (int i = 0; i < B_SIZE; i++) {
-                if (gameboard[i][B_SIZE - i - 1] == 1) {
+                if (gameboard[i][B_SIZE - i - 1] == PLAYER1) {
                     player1++;
-                }else if(gameboard[i][B_SIZE - i - 1] == 2) {
+                }else if(gameboard[i][B_SIZE - i - 1] == PLAYER2) {
                     player2++;
                 }
             }
             if (player1 == B_SIZE) {
-                return 1;
+                return PLAYER1;
             }else if (player2 == B_SIZE) {
-                return 2;
+                return PLAYER2;
             }
         }
         return 0;
